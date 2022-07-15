85755f744cecf717cd4d75f24646310932cf90f9
Dmitry Dudin <ddudin@harlemnext.com>
2022-07-15 17:52:20 +0200

    feat: add basic window test

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
+<script setup lang="ts">
+import CheckWindowSize from "./components/CheckWindowSize.vue";
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <CheckWindowSize />
   </div>
 </template>
---
+++ components/CheckWindowSize.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { onMounted, ref, reactive } from "vue";
+
+const innerSizes = ref({});
+const outerSizes = ref({});
+
+function onResize() {
+  innerSizes.value = { width: window.innerWidth, height: window.innerHeight };
+  outerSizes.value = { width: window.outerWidth, height: window.outerHeight };
+}
+
+onMounted(() => {
+  onResize();
+  window.addEventListener("resize", onResize);
+});
+</script>
+
+<template>
+  <h1>window test</h1>
+  <p>innerSizes = {{ innerSizes }}</p>
+  <p>outerSizes = {{ outerSizes }}</p>
+</template>
+
+<style lang="scss" scoped></style>
